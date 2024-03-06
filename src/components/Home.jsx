f85b75276b8a49169d08a37d7bff24384df5d177
VamsiHanumantu <108566661+VamsiHanumantu@users.noreply.github.com>
2024-03-06 23:41:11 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 
 import {HiArrowNarrowRight} from 'react-icons/hi'
+import {Link} from 'react-scroll'
 import hero from '../assets/Vamsi_Hanumantu.jpg'
 const Home = () => {
   return (
@@ -12,7 +13,7 @@ const Home = () => {
          <h2 className='text-4xl sm:text-3xl font-bold text-[#8892be]'>I am a Full Stack Developer </h2>
          {/* <p  className='text-[#8892be] py-4 max-w-[700px]'>I am full-stack developer specializing in building exceptional digital experiences. Currently, I am focused on building responsive full stack web applications</p> */}
          <div>
-          <button className='text-white border-2 group px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Work <span className='group-hover:rotate-90 duration-300'><HiArrowNarrowRight className='ml-3' /></span></button>
+         <Link to='work' smooth={true} duration={500}> <button className='text-white border-2 group px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Work <span className='group-hover:rotate-90 duration-300'><HiArrowNarrowRight className='ml-3' /></span></button></Link>
          </div>
         </div>
 
