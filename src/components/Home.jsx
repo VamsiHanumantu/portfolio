08e2912a7caf9e5b6592a467dad379bdeb9ea5b9
VamsiHanumantu <108566661+VamsiHanumantu@users.noreply.github.com>
2024-03-06 23:37:26 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
         <p className='text-[#EA5455] text-xl'>Hi, my name is</p>
          <h1 className='text-4xl sm:text-3xl font-bold text-[#ccd6f6]'>Vamsi Hanumantu</h1>
          <h2 className='text-4xl sm:text-3xl font-bold text-[#8892be]'>I am a Full Stack Developer </h2>
-         <p  className='text-[#8892be] py-4 max-w-[700px]'>I am full-stack developer specializing in building exceptional digital experiences. Currently, I am focused on building responsive full stack web applications</p>
+         {/* <p  className='text-[#8892be] py-4 max-w-[700px]'>I am full-stack developer specializing in building exceptional digital experiences. Currently, I am focused on building responsive full stack web applications</p> */}
          <div>
           <button className='text-white border-2 group px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Work <span className='group-hover:rotate-90 duration-300'><HiArrowNarrowRight className='ml-3' /></span></button>
          </div>
