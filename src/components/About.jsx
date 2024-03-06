f1be33d25a54fe1c555b4dbe335e6211727f3b04
VamsiHanumantu <108566661+VamsiHanumantu@users.noreply.github.com>
2024-03-06 23:33:44 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
                 <p className="sm:text-right text-4xl font-bold">Hi, I am Vamsi, nice to meet you. Please take look around</p>
             </div>
             <div>
-                <p>
+                <p className=" text-justify">
              I am eager to embark on a rewarding career as a Full Stack Developer, and I bring a solid foundation in web development coupled with hands-on experience. Proficient in front-end technologies such as HTML, CSS, and React, I specialize in creating responsive and interactive user interfaces that enhance the overall user experience. On the server side, I am adept at utilizing Node.js, Express, and MongoDB for efficient back-end development. My academic journey has been enriched by engaging in diverse projects which showcase my ability to conceptualize, design, and implement web applications. Committed to continuous learning, I stay updated on the latest industry trends and technologies, ensuring that my skills remain aligned with the dynamic landscape of web development. I am excited about the prospect of contributing my technical skills and creativity to a dynamic development team, fostering innovation, and making meaningful contributions to the field.</p>
             </div>
 
