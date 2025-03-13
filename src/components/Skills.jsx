2570cd90c8a0f0478737f22aad49c64b719757f4
VAMSI HANUMANTU <vamsihanumantu1729@gmail.com>
2025-03-14 00:00:15 +0530

    adjust heights

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,7 +10,7 @@ import java from '../assets/java.png'
 import python from '../assets/python.png'
 const Skills = () => {
   return (
-    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+    <div name='skills' className='w-full bg-[#0a192f] text-gray-300'>
 
         {/* container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
